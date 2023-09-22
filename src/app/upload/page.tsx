1f778fa1209f2fcbfc67bc33982ecdbb0f257d0a
TranDatk <hgooshvhd123@gmail.com>
2023-09-22 11:44:04 +0700

    update

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,20 @@
+/** @format */
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: 'Page | Upload',
+  description: 'This is upload page',
+}
+
+
+export default function Upload() {
+  return (
+    <>
+      <div>
+        <p>Day la trang upload</p>
+      </div>
+    </>
+  )
+}
